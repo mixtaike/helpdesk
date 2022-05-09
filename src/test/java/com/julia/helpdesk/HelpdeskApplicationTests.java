8425b8800f99077b370e415d0c7565a59f3a68ca
Mixtaike <mixtaike@gmail.com>
2022-05-09 10:17:54 -0300

    Criação do projeto

---
+++ src/test/java/com/julia/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julia.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
