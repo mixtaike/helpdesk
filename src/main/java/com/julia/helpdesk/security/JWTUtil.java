f3a380f29ae393a0193217f788ad2c4b58711d63
Mixtaike <mixtaike@gmail.com>
2022-05-10 14:24:59 -0300

    Criar classe JWTUtil (para gerar o token de autentição usando dados do aplication.properties

---
+++ src/main/java/com/julia/helpdesk/security/JWTUtil.java
@@ -0,0 +1,30 @@
+package com.julia.helpdesk.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JWTUtil {
+	
+	@Value("${jwt.expiration}")
+	private Long expiration;
+	
+	@Value("${jwt.secret}")
+	private String secret;
+	
+	//usado pra gerar o token de validação
+	public String generateToken(String email) {
+		return Jwts.builder()
+				.setSubject(email)
+				.setExpiration(new Date(System.currentTimeMillis()+expiration))
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
+				.compact();
+				
+	}
+
+}
