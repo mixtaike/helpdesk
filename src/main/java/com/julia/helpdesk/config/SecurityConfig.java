7bce3979296719b997fc9c4aa79e4c7f2237a536
Mixtaike <mixtaike@gmail.com>
2022-05-10 12:42:29 -0300

    Spring Security 1 - Conf CROSS permitindo acesso ao H2, e verbos HTTP após colocar depedency do spring security

---
+++ src/main/java/com/julia/helpdesk/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.julia.helpdesk.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+	
+	@Autowired
+	private Environment env;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+		http.cors().and().csrf().disable();
+
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	//@Bean
+	//CorsConfigurationSource corsConfigurationSource() {
+		//CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		//configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		//final CorsConfigurationSource source = (CorsConfigurationSource) new UrlBasedCorsConfigurationSource();
+		//((UrlBasedCorsConfigurationSource) source).registerCorsConfiguration("/**", configuration);
+		//return source;
+	
+	
+	//}
+	
+		UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+		
+	
+		
+}
