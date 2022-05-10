bf81da804368d08cae13871e9741523337effd3a
Mixtaike <mixtaike@gmail.com>
2022-05-10 11:14:46 -0300

    create para chamado

--- src/main/java/com/julia/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/julia/helpdesk/resources/ChamadoResource.java
@@ -1,18 +1,23 @@
 package com.julia.helpdesk.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.julia.helpdesk.domain.Chamado;
 import com.julia.helpdesk.domain.dtos.ChamadoDTO;
-import com.julia.helpdesk.domain.dtos.ClienteDTO;
 import com.julia.helpdesk.services.ChamadoService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -36,4 +41,12 @@ public class ChamadoResource {
 		List<ChamadoDTO> listDTO = list.stream(). map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO) throws ObjectNotFoundException {
+		Chamado obj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
--- src/main/java/com/julia/helpdesk/services/ChamadoService.java
+++ src/main/java/com/julia/helpdesk/services/ChamadoService.java
@@ -3,11 +3,19 @@ package com.julia.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.julia.helpdesk.domain.Chamado;
+import com.julia.helpdesk.domain.Cliente;
+import com.julia.helpdesk.domain.Tecnico;
+import com.julia.helpdesk.domain.dtos.ChamadoDTO;
+import com.julia.helpdesk.domain.enums.Prioridade;
+import com.julia.helpdesk.domain.enums.Status;
 import com.julia.helpdesk.repositories.ChamadoRepository;
+
 import javassist.tools.rmi.ObjectNotFoundException;
 
 @Service
@@ -16,6 +24,11 @@ public class ChamadoService {
 	@Autowired
 	private ChamadoRepository repository;
 	
+	@Autowired
+	private TecnicoService tecnicoService;
+	
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id) throws ObjectNotFoundException{
 		Optional<Chamado> obj = repository.findById(id);
@@ -27,6 +40,33 @@ public class ChamadoService {
 	public List<Chamado> findAll() {
 		
 		return repository.findAll();
+	}
+
+
+	public Chamado create(@Valid ChamadoDTO objDTO) throws ObjectNotFoundException {
+		
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) throws ObjectNotFoundException {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		
+		if (obj.getId() != null) {
+			chamado.setId(obj.getId());
+			
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
+		
 	}
 
 }
